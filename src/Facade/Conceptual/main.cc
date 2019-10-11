e64c597235ff9c120831fdee1f9fc7c6b14cf944
Eder Duran <ederduran@Eders-MacBook-Pro.local>
2019-10-11 10:57:00 -0500

    facade and decorator Pattern

---
+++ src/Facade/Conceptual/main.cc
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <string>
+
+/**
+ * EN: Facade Design Pattern
+ *
+ * Intent: Provides a simplified interface to a library, a framework, or any
+ * other complex set of classes.
+ *
+ * RU: Паттерн Фасад
+ *
+ * Назначение: Предоставляет простой интерфейс к сложной системе классов,
+ * библиотеке или фреймворку.
+ */
+
+/**
+ * EN: The Subsystem can accept requests either from the facade or client
+ * directly. In any case, to the Subsystem, the Facade is yet another client,
+ * and it's not a part of the Subsystem.
+ *
+ * RU: Подсистема может принимать запросы либо от фасада, либо от клиента
+ * напрямую. В любом случае, для Подсистемы Фасад – это еще один клиент, и он не
+ * является частью Подсистемы.
+ */
+class Subsystem1
+{
+public:
+    std::string Operation1() const
+    {
+        return "Subsystem1: Ready!\n";
+    }
+    // ...
+    std::string OperationN() const
+    {
+        return "Subsystem1: Go!\n";
+    }
+};
+/**
+ * EN: Some facades can work with multiple subsystems at the same time.
+ *
+ * RU: Некоторые фасады могут работать с разными подсистемами одновременно.
+ */
+class Subsystem2
+{
+public:
+    std::string Operation1() const
+    {
+        return "Subsystem2: Get ready!\n";
+    }
+    // ...
+    std::string OperationZ() const
+    {
+        return "Subsystem2: Fire!\n";
+    }
+};
+
+/**
+ * EN: The Facade class provides a simple interface to the complex logic of one
+ * or several subsystems. The Facade delegates the client requests to the
+ * appropriate objects within the subsystem. The Facade is also responsible for
+ * managing their lifecycle. All of this shields the client from the undesired
+ * complexity of the subsystem.
+ *
+ * RU: Класс Фасада предоставляет простой интерфейс для сложной логики одной или
+ * нескольких подсистем. Фасад делегирует запросы клиентов соответствующим
+ * объектам внутри подсистемы. Фасад также отвечает за управление их жизненным
+ * циклом. Все это защищает клиента от нежелательной сложности подсистемы.
+ */
+class Facade
+{
+protected:
+    Subsystem1 *subsystem1_;
+    Subsystem2 *subsystem2_;
+    /**
+     * EN: Depending on your application's needs, you can provide the Facade
+     * with existing subsystem objects or force the Facade to create them on its
+     * own.
+     *
+     * RU: В зависимости от потребностей вашего приложения вы можете
+     * предоставить Фасаду существующие объекты подсистемы или заставить Фасад
+     * создать их самостоятельно.
+     */
+public:
+    /**
+     * EN: In this case we will delegate the memory ownership to Facade Class
+     *
+     * RU: 
+     */
+    Facade(
+        Subsystem1 *subsystem1 = nullptr,
+        Subsystem2 *subsystem2 = nullptr)
+    {
+        this->subsystem1_ = subsystem1 ?: new Subsystem1;
+        this->subsystem2_ = subsystem2 ?: new Subsystem2;
+    }
+    ~Facade()
+    {
+        delete subsystem1_;
+        delete subsystem2_;
+    }
+    /**
+     * EN: The Facade's methods are convenient shortcuts to the sophisticated
+     * functionality of the subsystems. However, clients get only to a fraction
+     * of a subsystem's capabilities.
+     *
+     * RU: Методы Фасада удобны для быстрого доступа к сложной функциональности
+     * подсистем. Однако клиенты получают только часть возможностей подсистемы.
+     */
+    std::string Operation()
+    {
+        std::string result = "Facade initializes subsystems:\n";
+        result += this->subsystem1_->Operation1();
+        result += this->subsystem2_->Operation1();
+        result += "Facade orders subsystems to perform the action:\n";
+        result += this->subsystem1_->OperationN();
+        result += this->subsystem2_->OperationZ();
+        return result;
+    }
+};
+
+/**
+ * EN: The client code works with complex subsystems through a simple interface
+ * provided by the Facade. When a facade manages the lifecycle of the subsystem,
+ * the client might not even know about the existence of the subsystem. This
+ * approach lets you keep the complexity under control.
+ *
+ * RU: Клиентский код работает со сложными подсистемами через простой интерфейс,
+ * предоставляемый Фасадом. Когда фасад управляет жизненным циклом подсистемы,
+ * клиент может даже не знать о существовании подсистемы. Такой подход позволяет
+ * держать сложность под контролем.
+ */
+void ClientCode(Facade *facade)
+{
+    // ...
+    std::cout << facade->Operation();
+    // ...
+}
+/**
+ * EN: The client code may have some of the subsystem's objects already created.
+ * In this case, it might be worthwhile to initialize the Facade with these
+ * objects instead of letting the Facade create new instances.
+ *
+ * RU: В клиентском коде могут быть уже созданы некоторые объекты подсистемы. В
+ * этом случае может оказаться целесообразным инициализировать Фасад с этими
+ * объектами вместо того, чтобы позволить Фасаду создавать новые экземпляры.
+ */
+
+int main()
+{
+
+    Subsystem1 *subsystem1 = new Subsystem1;
+    Subsystem2 *subsystem2 = new Subsystem2;
+    Facade *facade = new Facade(subsystem1, subsystem2);
+    ClientCode(facade);
+
+    delete facade;
+
+    return 0;
+}
