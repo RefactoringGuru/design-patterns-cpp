ef3dcd764971af9c59933b976c667fbf749bd378
Eder Duran <ederduran@Eders-MacBook-Pro.local>
2019-10-19 18:15:17 -0500

    Memento design pattern

---
+++ src/Memento/Conceptual/main.cc
@@ -0,0 +1,253 @@
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+/**
+ * EN: Memento Design Pattern
+ *
+ * Intent: Lets you save and restore the previous state of an object without
+ * revealing the details of its implementation.
+ *
+ * RU: Паттерн Снимок
+ *
+ * Назначение: Фиксирует и восстанавливает внутреннее состояние объекта таким
+ * образом, чтобы в дальнейшем объект можно было восстановить в этом состоянии
+ * без нарушения инкапсуляции.
+ */
+
+/**
+ * EN: The Memento interface provides a way to retrieve the memento's metadata,
+ * such as creation date or name. However, it doesn't expose the Originator's
+ * state.
+ *
+ * RU: Интерфейс Снимка предоставляет способ извлечения метаданных снимка, таких
+ * как дата создания или название. Однако он не раскрывает состояние Создателя.
+ */
+class Memento
+{
+public:
+    virtual std::string GetName() const = 0;
+    virtual std::string date() const = 0;
+    virtual std::string state() const = 0;
+};
+
+/**
+ * EN: The Concrete Memento contains the infrastructure for storing the
+ * Originator's state.
+ *
+ * RU: Конкретный снимок содержит инфраструктуру для хранения состояния
+ * Создателя.
+ */
+class ConcreteMemento : public Memento
+{
+private:
+    std::string state_;
+    std::string date_;
+
+public:
+    ConcreteMemento(std::string state) : state_(state)
+    {
+        this->state_ = state;
+        std::time_t now = std::time(0);
+        this->date_ = std::ctime(&now);
+    }
+    /**
+     * EN: The Originator uses this method when restoring its state.
+     *
+     * RU: Создатель использует этот метод, когда восстанавливает своё
+     * состояние.
+     */
+    std::string state() const override
+    {
+        return this->state_;
+    }
+    /**
+     * EN: The rest of the methods are used by the Caretaker to display
+     * metadata.
+     *
+     * RU: Остальные методы используются Опекуном для отображения метаданных.
+     */
+    std::string GetName() const override
+    {
+        return this->date_ + " / (" + this->state_.substr(0, 9) + "...)";
+    }
+    std::string date() const override
+    {
+        return this->date_;
+    }
+};
+
+/**
+ * EN: The Originator holds some important state that may change over time. It
+ * also defines a method for saving the state inside a memento and another
+ * method for restoring the state from it.
+ *
+ * RU: Создатель содержит некоторое важное состояние, которое может со временем
+ * меняться. Он также объявляет метод сохранения состояния внутри снимка и метод
+ * восстановления состояния из него.
+ */
+class Originator
+{
+    /**
+     * EN: @var string For the sake of simplicity, the originator's state is
+     * stored inside a single variable.
+     *
+     * RU: @var string Для удобства состояние создателя хранится внутри одной
+     * переменной.
+     */
+private:
+    std::string state_;
+
+    std::string GenerateRandomString(int length = 10)
+    {
+        const char alphanum[] =
+            "0123456789"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz";
+        int stringLength = sizeof(alphanum) - 1;
+
+        std::string random_string;
+        for (int i = 0; i < length; i++)
+        {
+            random_string += alphanum[std::rand() % stringLength];
+        }
+        return random_string;
+    }
+
+public:
+    Originator(std::string state) : state_(state)
+    {
+        std::cout << "Originator: My initial state is: " << this->state_ << "\n";
+    }
+    /**
+     * EN: The Originator's business logic may affect its internal state.
+     * Therefore, the client should backup the state before launching methods of
+     * the business logic via the save() method.
+     *
+     * RU: Бизнес-логика Создателя может повлиять на его внутреннее состояние.
+     * Поэтому клиент должен выполнить резервное копирование состояния с помощью
+     * метода save перед запуском методов бизнес-логики.
+     */
+    void DoSomething()
+    {
+        std::cout << "Originator: I'm doing something important.\n";
+        this->state_ = this->GenerateRandomString(30);
+        std::cout << "Originator: and my state has changed to: " << this->state_ << "\n";
+    }
+
+    /**
+     * EN: Saves the current state inside a memento.
+     *
+     * RU: Сохраняет текущее состояние внутри снимка.
+     */
+    Memento *Save()
+    {
+        return new ConcreteMemento(this->state_);
+    }
+    /**
+     * EN: Restores the Originator's state from a memento object.
+     *
+     * RU: Восстанавливает состояние Создателя из объекта снимка.
+     */
+    void Restore(Memento *memento)
+    {
+        this->state_ = memento->state();
+        std::cout << "Originator: My state has changed to: " << this->state_ << "\n";
+    }
+};
+
+/**
+ * EN: The Caretaker doesn't depend on the Concrete Memento class. Therefore, it
+ * doesn't have access to the originator's state, stored inside the memento. It
+ * works with all mementos via the base Memento interface.
+ *
+ * RU: Опекун не зависит от класса Конкретного Снимка. Таким образом, он не
+ * имеет доступа к состоянию создателя, хранящемуся внутри снимка. Он работает
+ * со всеми снимками через базовый интерфейс Снимка.
+ */
+class Caretaker
+{
+    /**
+     * @var Memento[]
+     */
+private:
+    std::vector<Memento *> mementos_;
+
+    /**
+     * @var Originator
+     */
+    Originator *originator_;
+
+public:
+    Caretaker(Originator *originator) : originator_(originator)
+    {
+        this->originator_ = originator;
+    }
+
+    void Backup()
+    {
+        std::cout << "\nCaretaker: Saving Originator's state...\n";
+        this->mementos_.push_back(this->originator_->Save());
+    }
+    void Undo()
+    {
+        if (!this->mementos_.size())
+        {
+            return;
+        }
+        Memento *memento = this->mementos_.back();
+        this->mementos_.pop_back();
+        std::cout << "Caretaker: Restoring state to: " << memento->GetName() << "\n";
+        try
+        {
+            this->originator_->Restore(memento);
+        }
+        catch (...)
+        {
+            this->Undo();
+        }
+    }
+    void ShowHistory() const
+    {
+        std::cout << "Caretaker: Here's the list of mementos:\n";
+        for (Memento *memento : this->mementos_)
+        {
+            std::cout << memento->GetName() << "\n";
+        }
+    }
+};
+/**
+ * EN: Client code.
+ *
+ * RU: Клиентский код.
+ */
+
+void ClientCode()
+{
+    Originator *originator = new Originator("Super-duper-super-puper-super.");
+    Caretaker *caretaker = new Caretaker(originator);
+    caretaker->Backup();
+    originator->DoSomething();
+    caretaker->Backup();
+    originator->DoSomething();
+    caretaker->Backup();
+    originator->DoSomething();
+    std::cout << "\n";
+    caretaker->ShowHistory();
+    std::cout << "\nClient: Now, let's rollback!\n\n";
+    caretaker->Undo();
+    std::cout << "\nClient: Once more!\n\n";
+    caretaker->Undo();
+
+    delete originator;
+    delete caretaker;
+}
+
+int main()
+{
+    std::srand(static_cast<unsigned int>(std::time(NULL)));
+    ClientCode();
+    return 0;
+}
