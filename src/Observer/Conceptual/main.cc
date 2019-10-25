3534216b81cca635c4b06bd8e6c712fe191b3a69
Eder Duran <ederduran@Eders-MacBook-Pro.local>
2019-10-24 19:06:38 -0500

    Observer design pattern

---
+++ src/Observer/Conceptual/main.cc
@@ -0,0 +1,194 @@
+/**
+ * EN: Observer Design Pattern
+ *
+ * Intent: Lets you define a subscription mechanism to notify multiple objects
+ * about any events that happen to the object they're observing.
+ *
+ * Note that there's a lot of different terms with similar meaning associated
+ * with this pattern. Just remember that the Subject is also called the
+ * Publisher and the Observer is often called the Subscriber and vice versa.
+ * Also the verbs "observe", "listen" or "track" usually mean the same thing.
+ *
+ * RU: Паттерн Наблюдатель
+ *
+ * Назначение: Создаёт механизм подписки, позволяющий одним объектам следить и
+ * реагировать на события, происходящие в других объектах.
+ *
+ * Обратите внимание, что существует множество различных терминов с похожими
+ * значениями, связанных с этим паттерном. Просто помните, что Субъекта также
+ * называют Издателем, а Наблюдателя часто называют Подписчиком и наоборот.
+ * Также глаголы «наблюдать», «слушать» или «отслеживать» обычно означают одно и
+ * то же.
+ */
+
+#include <list>
+#include <string>
+#include <iostream>
+
+class IObserver
+{
+
+public:
+    virtual ~IObserver(){};
+    virtual void Update(const std::string &message_from_subject) = 0;
+};
+
+class ISubject
+{
+public:
+    virtual ~ISubject(){};
+    virtual void Attach(IObserver *observer) = 0;
+    virtual void Detach(IObserver *observer) = 0;
+    virtual void Notify() = 0;
+};
+
+/**
+ * EN: The Subject owns some important state and notifies observers when the
+ * state changes.
+ *
+ * RU: Издатель владеет некоторым важным состоянием и оповещает наблюдателей о
+ * его изменениях.
+ */
+
+class Subject : public ISubject
+{
+
+public:
+    virtual ~Subject()
+    {
+        std::cout << "Goodbye, I was the Subject.\n";
+    }
+
+    /**
+     * EN: The subscription management methods.
+     *
+     * RU: Методы управления подпиской.
+     */
+    void Attach(IObserver *observer) override
+    {
+        list_observer_.push_back(observer);
+    }
+    void Detach(IObserver *observer) override
+    {
+        list_observer_.remove(observer);
+    }
+    void Notify() override
+    {
+        std::list<IObserver *>::iterator iterator = list_observer_.begin();
+        HowManyObserver();
+        while (iterator != list_observer_.end())
+        {
+            (*iterator)->Update(message_);
+            ++iterator;
+        }
+    }
+
+    void CreateMessage(std::string message = "Empty")
+    {
+        this->message_ = message;
+        Notify();
+    }
+    void HowManyObserver()
+    {
+        std::cout << "There are " << list_observer_.size() << " observers in the list.\n";
+    }
+
+    /**
+     * EN: Usually, the subscription logic is only a fraction of what a Subject
+     * can really do. Subjects commonly hold some important business logic, that
+     * triggers a notification method whenever something important is about to
+     * happen (or after it).
+     *
+     * RU: Обычно логика подписки – только часть того, что делает Издатель.
+     * Издатели часто содержат некоторую важную бизнес-логику, которая запускает
+     * метод уведомления всякий раз, когда должно произойти что-то важное (или
+     * после этого).
+     */
+    void SomeBusinessLogic()
+    {
+        this->message_ = "change message message";
+        Notify();
+        std::cout << "I'm about to do some thing important\n";
+    }
+
+private:
+    std::list<IObserver *> list_observer_;
+    std::string message_;
+};
+
+class Observer : public IObserver
+{
+
+public:
+    Observer(Subject &subject) : subject_(subject)
+    {
+        this->subject_.Attach(this);
+        std::cout << "Hi, I'm the Observer \"" << ++Observer::static_number_ << "\".\n";
+        this->number_ = Observer::static_number_;
+    }
+    virtual ~Observer()
+    {
+        std::cout << "Goodbye, I was the Observer \"" << this->number_ << "\".\n";
+    }
+
+    void Update(const std::string &message_from_subject) override
+    {
+        message_from_subject_ = message_from_subject;
+        PrintInfo();
+    }
+    void RemoveMeFromTheList()
+    {
+        subject_.Detach(this);
+        std::cout << "Observer \"" << number_ << "\" removed from the list.\n";
+    }
+    void PrintInfo()
+    {
+        std::cout << "Observer \"" << this->number_ << "\": a new message is available --> " << this->message_from_subject_ << "\n";
+    }
+
+private:
+    std::string message_from_subject_;
+    Subject &subject_;
+    static int static_number_;
+    int number_;
+};
+
+int Observer::static_number_ = 0;
+
+void ClientCode()
+{
+    Subject *subject = new Subject;
+    Observer *observer1 = new Observer(*subject);
+    Observer *observer2 = new Observer(*subject);
+    Observer *observer3 = new Observer(*subject);
+    Observer *observer4;
+    Observer *observer5;
+
+    subject->CreateMessage("Hello World! :D");
+    observer3->RemoveMeFromTheList();
+
+    subject->CreateMessage("The weather is hot today! :p");
+    observer4 = new Observer(*subject);
+
+    observer2->RemoveMeFromTheList();
+    observer5 = new Observer(*subject);
+
+    subject->CreateMessage("My new car is great! ;)");
+    observer5->RemoveMeFromTheList();
+
+    observer4->RemoveMeFromTheList();
+    observer1->RemoveMeFromTheList();
+
+    delete observer5;
+    delete observer4;
+    delete observer3;
+    delete observer2;
+    delete observer1;
+    delete subject;
+}
+
+int main()
+{
+    ClientCode();
+    return 0;
+}
